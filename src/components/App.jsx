75f1d80ee7b88a60f026abc8c5c7c7b004f71794
Anton K <akarpenko83@gmail.com>
2023-05-26 16:52:05 +0300

    initial project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+class App extends Component {
+    state = {};
+
+    render() {
+        return 'return';
+    }
+}
+export default App;
